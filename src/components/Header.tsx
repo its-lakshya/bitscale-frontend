bfe872b13b4fde333fa258fb76303ef76c3da532
Lakshya Kumar <kumarlakshya101@gmail.com>
2026-01-13 00:44:30 +0530

    feat: add application layout and header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import cloudIcon from "../assets/icons/cloud.svg";
+import coinsIcon from "../assets/icons/coins.svg";
+import homeIcon from "../assets/icons/home.svg";
+import rocketIcon from "../assets/icons/rocket.svg";
+import starIcon from "../assets/icons/star.svg";
+
+const Header = () => {
+  return (
+    <header className="w-full px-6 py-2 flex items-center justify-between text-sm font-medium">
+      <div className="flex items-center gap-4">
+        <div className="size-6 flex justify-center items-center rounded-sm bg-[#F3F4F6]">
+          <Image src={homeIcon} alt="home icon" />
+        </div>
+        <div className="flex items-center gap-4">
+          <Image src={starIcon} alt="star icon" />
+          <div className="flex items-center gap-2">
+            <span className="text-[#9CA3AF]">
+              Workbook - Bitscale UX/UI testing flow
+            </span>
+            <span className="text-[#9CA3AF]">/</span>
+            <span className="text-[#374151]">Bitscale grid only</span>
+          </div>
+        </div>
+        {/* <div className="flex items-center gap-2 ml-4">
+          <span className="text-sm text-gray-600">Grid running</span>
+          <div className="w-28 h-2 rounded-full bg-gray-200 overflow-hidden">
+            <div className="h-full w-1/3 bg-blue-500" />
+          </div>
+          <span className="text-sm text-gray-600">10%</span>
+        </div> */}
+      </div>
+
+      <div className="flex items-center gap-4">
+        <button className="flex items-center gap-1.5 px-3 py-1 bg-[#FBF3DB] text-[#CB912E] rounded-2xl hover:scale-105 translate-full duration-300 cursor-pointer">
+          <Image src={rocketIcon} alt="rocket icon" />
+          Upgrade Plan
+        </button>
+        <Image src={cloudIcon} alt="cloud icon" />
+        <button className="flex items-center gap-4 px-2 py-1 bg-[#EDF3EC] text-xs border border-[#98BCA6] rounded-[10px] hover:scale-105 translate-full duration-300 cursor-pointer">
+          <div className='flex items-center gap-1.5'>
+            <Image src={coinsIcon} alt="coins icon" />
+            <span className="text-[#438361] ">500/500</span>
+          </div>
+          <span className="w-10 h-5 flex items-center justify-center text-white bg-[#438361] rounded-lg">
+            Free
+          </span>
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
